f0ebc1bf71df8251812670001832d388a0f403ff
omertiser <omer.tiser@gmail.com>
2024-02-09 13:00:06 +0200

    נראה לי עובד

--- ex5.py
+++ ex5.py
@@ -57,6 +57,7 @@ class Mutase(Enzyme):
     
     def process(self, dna_sequence):
         dna_sequence.m_nucleotides[0::int(self.m_freq)] = dna_sequence.get_complement()[0::int(self.m_freq)]
+        return dna_sequence
 
 class Polymerase(Enzyme):
     def process(self, dna_sequence):
@@ -69,6 +70,7 @@ class CRISPR(Enzyme):
 
     def process(self, dna_sequence):
         replace_sub_lists(dna_sequence, self.m_seq, ['W'])
+        return dna_sequence
 
 
 class CRISPR_Cas9(CRISPR):
@@ -81,6 +83,7 @@ class CRISPR_Cas9(CRISPR):
         to_replace = [nuc for nuc in self.m_new_seq]
         replace_sub_lists(dna_sequence, 'W', to_replace)
         replace_sub_lists(dna_sequence, 'M', DNASequence(to_replace).get_complement())
+        return dna_sequence
 
 def processData(dir_path):
     name_index = 0
@@ -109,7 +112,7 @@ def processData(dir_path):
             enzyme = CRISPR(arguments[first_argument])
         if arguments[enzyme_index] == "CRISPR/Cas9":
             enzyme = CRISPR_Cas9(arguments[first_argument], arguments[second_argument])
-        enzyme.process(loaded[arguments[name_index]])
+        loaded[arguments[name_index]] = enzyme.process(loaded[arguments[name_index]])
         loaded[arguments[name_index]] = list_to_string(loaded[arguments[name_index]].get_sequence())
 
     name = dir_path + '/ModifiedDNA.json'
